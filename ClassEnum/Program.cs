9f136ac38a42baaf3b07669b3e7f412e0282824a
Scott Kuhl <scott@kuhl.ws>
2018-09-22 15:50:12 -0500

    Add project files.

---
+++ ClassEnum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
